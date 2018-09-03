7ad6621aa5bff82865078bb1d49c75fa01bbdf97
kartavtcev <kartavtcev.sergei@gmail.com>
2018-09-03 03:04:47 +0300

    Code cleanup.

--- src/main/scala/example/MeetupApp.scala
+++ src/main/scala/example/MeetupApp.scala
@@ -7,48 +7,55 @@ import monix.execution.CancelableFuture
 import monix.reactive.Observable
 
 import scala.concurrent.Await
-import scala.concurrent.duration._
+import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success}
 
 
-object MeetupApp extends StrictLogging with App {
+object MeetupApp extends StrictLogging {
 
-  val t0 = System.nanoTime()
+  def main(args: Array[String]) = {
 
-  implicit val ctx = monix.execution.Scheduler.Implicits.global
+    if(args.length != 2) {    // https://stackoverflow.com/questions/2066307/how-do-you-input-commandline-argument-in-intellij-idea
+      println("Input 2 ids.")
+    } else {
+      val t0 = System.nanoTime()
 
-  val id1 = (1, "655f7545")
-  val id2 = (2, "78b85537")
-  val parser = new Parser(id1, id2)
-  val fileName = "reduced.csv"
+      implicit val ctx = monix.execution.Scheduler.Implicits.global
 
-  val reader = new BufferedReader(new InputStreamReader(
-    new FileInputStream(fileName), "UTF-8"))
+      val id1 = (1, args(0))
+      val id2 = (2, args(1))
+      val parser = new Parser(id1, id2)
+      val fileName = "reduced.csv"
 
-  lazy val cf : CancelableFuture[Unit] =
-    Observable.fromLinesReader(reader)
-      .drop(1)
-      .filter(parser.filterPred)
-      .map(parser.parse)
-      .collect {
-        case Success(t) => Some(t)
-        case Failure(e) =>
-          logger.error(e.toString)
-          None
-      }
-      .collect{ case Some(t) => t }
-      // 8458 records for 2 IDs: 655f7545, 78b85537
-      // more often: replace 655f7545 with 5e7b40e1    ; two all different coordinates: 600dfbe2    & 3c3649fb /    285d22e4 / 74d917a1 /
-      // NOT FOUND INTERSECTION: 285d22e4 / 74d917a1
-      // p.s. try 600dfbe2 & 5e7b40e1
-      .consumeWith(ProcessorConsumer.value)
-      .foreach{
-        case  Right(list) => list.foreach(println)
-        case Left(msg) => println(msg)
-      }
+      val reader = new BufferedReader(new InputStreamReader(
+        new FileInputStream(fileName), "UTF-8"))
 
-  Await.result(cf, Duration.Inf)
+      lazy val cf: CancelableFuture[Unit] =
+        Observable.fromLinesReader(reader)
+          .drop(1)
+          .filter(parser.filterPred)
+          .map(parser.parse)
+          .collect {
+            case Success(t) => Some(t)
+            case Failure(e) =>
+              logger.error(e.toString)
+              None
+          }
+          .collect { case Some(t) => t }
+          .consumeWith(ProcessorConsumer.value)
+          .foreach {
+            case Right(list) => list.foreach(println)
+            case Left(msg) => println(msg)
+          }
 
-  val t1 = System.nanoTime()
-  println(s"Elapsed run-time: ${(t1-t0)/1000000} ms")
+      Await.result(cf, Duration.Inf)
+
+      val t1 = System.nanoTime()
+      println(s"Elapsed run-time: ${(t1 - t0) / 1000000} ms")
+    }
+  }
 }
+/*
+8458 records for 2 IDs: 655f7545, 78b85537
+NOT FOUND INTERSECTION: 285d22e4 / 74d917a1
+*/
